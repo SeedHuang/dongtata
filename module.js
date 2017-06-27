26f6998c456fbd353a8f9b14bf609e2faca4a811
huangchunhua <huangchunhua@baidu.com>
2017-06-27 15:43:03 +0800

    add module.js package.json widget.js

---
+++ module.js
@@ -0,0 +1,36 @@
+(function(){
+    !window.Bdbrowser && (window.Bdbrowser = {});
+    var moduleSet = {};
+    var notinitSet = {};
+    function define(key,module,isRun){
+        if(notinitSet[key]){
+            throw "[widget define error] "+key+" has alrealy been named";
+        }
+        notinitSet[key] = module;
+        if(isRun){
+            moduleSet[key] = notinitSet[key](require);
+        }
+    }
+
+    function require(key){
+        if(moduleSet[key]){
+            return moduleSet[key];
+        }
+        else{
+            if(!notinitSet[key]){
+                throw "You haven't a module called "+key;
+            }
+
+            if(notinitSet[key].call){
+                moduleSet[key] = notinitSet[key](require);
+            }
+            else{
+                moduleSet[key] = notinitSet[key];
+            }
+            return moduleSet[key];
+        }
+    }
+    window.Bdbdefine = define;
+    Bdbrowser.define = define;
+    Bdbrowser.require = require;
+})();
---
+++ widget.js
@@ -0,0 +1,55 @@
+Bdbdefine("Bd:Widget", {
+    "extend": function(options) {
+        function Widget(opts) {
+            var self = this;
+            for(var field in opts){
+                self[field] = opts[field];
+            }
+            if (!self.tagName)
+                throw "[Comment Widget Error] Need tagName";
+            self.el = document.createElement(self.tagName);
+            self.$el = $(self.el);
+            self.className && (self.el.className = self.className);
+            self.id && (self.el.id = self.id);
+            if (self.events) {
+                var keys = Object.keys(self.events);
+                var keylength = keys.length;
+                var i=0;
+                while(i < keylength){
+                    var field = keys[i];
+                    var parts = field.split(" ");
+                    var eventname = "";
+                    var selectors = [];
+                    switch (parts.length) {
+                        case 0:
+                            break;
+                        case 1:
+                            (function($el, eName,  func, context){
+                                $el.on(eName, function(event){
+                                    func.call(context, event);
+                                });
+                            })(self.$el, parts[0], self.events[field], self);
+                            break;
+                        default:
+                            var eventName = parts.shift();
+                            var eventSelector = parts.join(" ");
+                            (function($el, eName, selector, func, context){
+                                $el.on(eName, selector, function(event){
+                                    func.call(context, event);
+                                });
+                            })(self.$el, eventName, eventSelector, self.events[field], self);
+                            break;
+                    }
+                    i++;
+                }
+                
+            }
+            if (!self.create) {
+                throw "[Comment Widget Error] Need create function";
+            }
+            self.create();
+        };
+        Widget.prototype = options;
+        return Widget;
+    }
+});
